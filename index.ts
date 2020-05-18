ff29459e233cf5b364c5786930dfdfbf38fa6f08
cuminzh <cuminzh@wicrenet.com>
2020-05-18 19:09:34 +0800

    first commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+function getInfo(name: string, age: number): string {
+    console.log
+    return name
+}
+
+alert(getInfo('zhang', 20))
---
+++ js/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function getInfo(name, age) {
+    console.log;
+    return name;
+}
+alert(getInfo('zhang', 20));
