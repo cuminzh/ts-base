11f04d2b96da71c439f096907945c139719b9a66
zhanghuimin <cuminzh@wicrenet.com>
2020-05-19 22:12:44 +0800

    ts 封装ajax

--- index 接口.ts
+++ index 接口.ts
@@ -63,6 +63,24 @@ interface Config{
     dataType: string;
 }
 //原生js封装ajax
-function ajax(){
-
+function ajax(config:Config){
+  var xhr= new XMLHttpRequest();
+  xhr.open(config.type, config.url, true)
+  xhr.send(config.data)
+  xhr.onreadystatechange=function(){
+    if(xhr.readyState==4&&xhr.status===200){
+      
+      if(config.dataType==='json'){
+        console.log(JSON.parse(xhr.responseText))
+      }else{
+        console.log(xhr.responseText)
+      }
+    }
+  }
 }
+
+ajax({
+  type:'get',
+  url: 'http://a.itying.com/api/productlist',
+  dataType: 'json'
+})
--- js/index 接口.js
+++ js/index 接口.js
@@ -5,5 +5,23 @@
 某些方法，提供这些方法的类就可以满足实际需要。ts中的接口类似于java，同时还增加了更灵活的接口类型，包括属性、函数、可索引和类等
 */
 //原生js封装ajax
-function ajax() {
+function ajax(config) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(config.type, config.url, true);
+    xhr.send(config.data);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status === 200) {
+            if (config.dataType === 'json') {
+                console.log(JSON.parse(xhr.responseText));
+            }
+            else {
+                console.log(xhr.responseText);
+            }
+        }
+    };
 }
+ajax({
+    type: 'get',
+    url: 'http://a.itying.com/api/productlist',
+    dataType: 'json'
+});
