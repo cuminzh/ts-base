95a149b9f73f7d54c4d77a4e0e7aafeed0d9acea
cuminzh <cuminzh@wicrenet.com>
2020-05-20 13:14:20 +0800

    ts 接口

--- index 接口.ts
+++ index 接口.ts
@@ -55,7 +55,7 @@ var obj = { //传入的参数必须包含 firstName secondName
 printName(obj); 
 */
 
-
+/*
 interface Config{
     type:string;
     url:string;
@@ -84,3 +84,153 @@ ajax({
   url: 'http://a.itying.com/api/productlist',
   dataType: 'json'
 })
+*/
+
+
+
+/*
+//函数类型接口：对方法传入的参数以及返回值进行约束
+
+
+//加密的函数类型接口
+
+interface encrypt {
+    (key: string, value: string): string;
+}
+
+var md5: encrypt = (key1: string, value1: string): string => {
+    return key1 + '---' + value1;
+}
+
+console.log(md5('name', '张三'));
+*/
+
+
+//可索引接口：数组、对象的约束 （不常用）
+
+//ts定义数组的方式
+/*
+var arr:number[]=[11, 22]
+
+var arr1:Array<string>=['aa', 'dd']
+*/
+
+//可索引接口 对数组的约束
+
+// interface userArr{
+//     [index:number]:string
+// }
+// var arr:userArr=['aa', 'bb']
+
+// console.log(arr[0])
+
+//可索引接口 对对象的约束
+
+// interface userObj {
+//     [index: string]: string
+// }
+// var obj: userObj = { name: '张三', mobile: '12234' }
+
+/*
+//类类型接口：对类的约束 和 抽象类有点相似
+
+interface Animal {
+    name: string;
+    eat(str: string): void;
+}
+
+class Dog implements Animal {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+    eat() {
+        console.log(this.name + '吃粮食'); 
+    }
+}
+
+var d = new Dog('小狗');
+d.eat();
+
+class Cat implements Animal {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+    eat(str: string) {
+        console.log(this.name + str);
+    }
+}
+
+var c = new Cat('小猫');
+c.eat('老鼠');
+*/
+
+//接口扩展：接口可以继承接口
+
+// interface Animal {
+//     eat(): void;
+// }
+
+// interface Person extends Animal {
+//     work(): void;
+// }
+
+// class Web implements Person {
+//     name: string;
+//     constructor(name: string) {
+//         this.name = name;
+//     }
+
+//     work() {
+//         console.log(this.name + '写代码');
+//     }
+
+//     eat() {
+//         console.log(this.name + '喝果汁');
+//     }
+// }
+
+// var w = new Web('小强');
+// w.work();
+// w.eat();
+
+
+
+interface Animal {
+    eat(): void;
+}
+
+interface Person extends Animal {
+    work(): void;
+}
+
+class Programmer{
+    public name:string;
+    constructor(name:string){
+        this.name=name;
+    }
+
+    coding(code:string):void{
+        console.log(this.name+code)
+    }
+}
+
+class Web extends Programmer implements Person {
+    constructor(name: string) {
+        super(name)
+    }
+
+    work() {
+        console.log(this.name + '写代码');
+    }
+
+    eat() {
+        console.log(this.name + '喝果汁');
+    }
+}
+
+var w = new Web('小强');
+w.work();
+w.eat();
+w.coding('写ts');
--- js/index 接口.js
+++ js/index 接口.js
@@ -4,24 +4,42 @@
 接口定义了某一批类所需要遵守的规范，接口不关心这些类的内部状态数据，也不关心这些类里方法的实现细节，它只规范这批类里必须提供
 某些方法，提供这些方法的类就可以满足实际需要。ts中的接口类似于java，同时还增加了更灵活的接口类型，包括属性、函数、可索引和类等
 */
-//原生js封装ajax
-function ajax(config) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(config.type, config.url, true);
-    xhr.send(config.data);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status === 200) {
-            if (config.dataType === 'json') {
-                console.log(JSON.parse(xhr.responseText));
-            }
-            else {
-                console.log(xhr.responseText);
-            }
-        }
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
     };
-}
-ajax({
-    type: 'get',
-    url: 'http://a.itying.com/api/productlist',
-    dataType: 'json'
-});
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Programmer = /** @class */ (function () {
+    function Programmer(name) {
+        this.name = name;
+    }
+    Programmer.prototype.coding = function (code) {
+        console.log(this.name + code);
+    };
+    return Programmer;
+}());
+var Web = /** @class */ (function (_super) {
+    __extends(Web, _super);
+    function Web(name) {
+        return _super.call(this, name) || this;
+    }
+    Web.prototype.work = function () {
+        console.log(this.name + '写代码');
+    };
+    Web.prototype.eat = function () {
+        console.log(this.name + '喝果汁');
+    };
+    return Web;
+}(Programmer));
+var w = new Web('小强');
+w.work();
+w.eat();
+w.coding('写ts');
